5864d0d3e438cda70d1c27719581ac399a24cdf4
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-11-08 13:34:21 -0700

    Added custom steps and fixed code to go with them.

--- AJ_CSP/Controller/AlgorithmsView.swift
+++ AJ_CSP/Controller/AlgorithmsView.swift
@@ -18,12 +18,16 @@ class AlgorithmsView: UIViewController {
         
         //TODO: Define algorithm and all steps
         let algorithm :String = "These are the instructions to create a project in Java using Eclipse and Github \n"
-        let stepOne :String = "First, ...."
-        let stepTwo :String = "Second, ...."
-        let stepThree :String = ".... ..."
+        let stepOne :String = "First, launch Eclipse and Github cause you’re going to need both."
+        let stepTwo :String = "Second, click file, new, Java project and name the project then click enter."
+        let stepThree :String = "Third, Go to Github and click file, new, repository and name it the same as the Java project and navigate to the folder the Java project is then click create repository."
+        let stepFour :String = "Fouth, Then click undo in the bottom left of Github then repository, repository settings, ignored files and add bin/ , .DS_Store , and *.class to the ignored files and click save."
+        let stepFive :String = "Fifth, Make your first commit message what you changed in the .gitignore file."
+        let stepSix :String = "Sixth, Go to Java and make a controller package and put: controller class, runner class in it."
+        let stepSeven :String = "Seventh, Make a model package, test package, and view package."
         
         //TODO: Finish adding all steps to the algorithm
-        algorithmSteps = [stepOne, stepTwo, stepThree]
+        algorithmSteps = [stepOne, stepTwo, stepThree, stepFour, stepFive, stepSix, stepSeven]
         let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
         let fullAttributedString = NSMutableAttributedString(string: algorithm, attributes: attributesDictionary)
        
