cc3408de48986a3db0c54fc5b94f817d27673183
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-11-08 13:27:59 -0700

    Added private func createParagraphStyle and private func setupAlgorithm.

--- AJ_CSP/Controller/AlgorithmsView.swift
+++ AJ_CSP/Controller/AlgorithmsView.swift
@@ -10,26 +10,59 @@ import UIKit
 
 class AlgorithmsView: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var algorithmText: UILabel!
+    
+    private func setupAlgorithm() -> Void
+    {
+        var algorithmSteps : [String] = []
+        
+        //TODO: Define algorithm and all steps
+        let algorithm :String = "These are the instructions to create a project in Java using Eclipse and Github \n"
+        let stepOne :String = "First, ...."
+        let stepTwo :String = "Second, ...."
+        let stepThree :String = ".... ..."
+        
+        //TODO: Finish adding all steps to the algorithm
+        algorithmSteps = [stepOne, stepTwo, stepThree]
+        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributedString(string: algorithm, attributes: attributesDictionary)
+       
+        for step in algorithmSteps
+        {
+            let bullet :String = "🖤"
+            let formattedStep :String = "\n\(bullet) \(step)"
+            let attributedStringStep: NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
+            let paragraphStyle = createParagraphStyle()
+            
+            attributedStringStep.addAttributes([NSAttributedStringKey.paragraphStyle : paragraphStyle], range:
+                NSMakeRange(0,attributedStringStep.length))
+            
+            fullAttributedString.append(attributedStringStep)
+        }
+        
+        algorithmText.attributedText = fullAttributedString
+    }
+    
+    private func createParagraphStyle() -> NSParagraphStyle
+    {
+        let paragraphStyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .left
+        paragraphStyle.defaultTabInterval = 15
+        paragraphStyle.firstLineHeadIndent = 20
+        paragraphStyle.headIndent = 35
+        
+        return paragraphStyle
+    }
+    
+    override public func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
