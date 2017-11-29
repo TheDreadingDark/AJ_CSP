8e1166bc5a70caaf01f26abeb1122d25f6992eba
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-11-29 11:53:31 -0700

    Added code for detail view controller, view did load, number of sections, and table view.

--- AJ_CSP/Controller/InternetMasterViewController.swift
+++ AJ_CSP/Controller/InternetMasterViewController.swift
@@ -38,6 +38,27 @@ public class InternetMasterViewController : UITableViewController
     }
 
     //MARK: - Table view data source
+   
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    ovveride public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return internetTopics.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel!.text = currentText
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
