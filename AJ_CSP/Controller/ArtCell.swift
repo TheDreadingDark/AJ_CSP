afacd207f203ab4ad48465e21ab8edcf497ad93f
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-12-07 12:53:48 -0700

    Created new class art cell and added 2 ib outlets, image view and image name.

---
+++ AJ_CSP/Controller/ArtCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCell.swift
+//  AJ_CSP
+//
+//  Created by Woolsey, Adam on 12/7/17.
+//  Copyright © 2017 Woolsey, Adam. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageName: UILabel!
+}
