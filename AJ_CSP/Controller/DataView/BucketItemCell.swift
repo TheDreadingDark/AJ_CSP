3796bcdac15702b08be1843e1ceee58571c25506
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-01-09 12:13:32 -0700

    Added framework.

---
+++ AJ_CSP/Controller/DataView/BucketItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  BucketItemCell.swift
+//  AJ_CSP
+//
+//  Created by Woolsey, Adam on 1/9/18.
+//  Copyright © 2018 Woolsey, Adam. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell
+{
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AJ_CSP/Model/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  AJ_CSP
+//
+//  Created by Woolsey, Adam on 1/9/18.
+//  Copyright © 2018 Woolsey, Adam. All rights reserved.
+//
+
+import Foundation
